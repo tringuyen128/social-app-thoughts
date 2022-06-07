7c87a0420ab4000f401e2fc0ffec574b69005d51
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-06-07 10:07:42 -0700

    create thoughlist compo

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React from 'react'
+//import useQuery hook from Aollo Client to make request to server
+// make it avaialbe to use ApolloProvider
+import { useQuery } from '@apollo/client'
+//import hook to render
+import { QUERY_THOUGHTS } from '../utils/queries'
+import ThoughtList from '../components/ThoughtList'
 
 const Home = () => {
+  // use useQuery hook to make query request
+  const { loading, data } = useQuery(QUERY_THOUGHTS)
+  // if data exists, store it in the thoughts constant we just created
+  const thoughts = data?.thoughts || []
+  console.log(thoughts)
+
   return (
     <main>
-      <div className='flex-row justify-space-between'>
-        <div className='col-12 mb-3'>{/* PRINT THOUGHT LIST */}</div>
+      <div className="flex-row justify-space-between">
+        <div className="col-12 mb-3">
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <ThoughtList
+              thoughts={thoughts}
+              title="Some Created Thought(s)..."
+            />
+          )}
+        </div>
       </div>
     </main>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
---
+++ client/src/utils/queries.js
@@ -0,0 +1,20 @@
+import { gql } from '@apollo/client'
+
+//this syntax is similiar with testing on Apollo browser
+export const QUERY_THOUGHTS = gql`
+  query thoughts($username: String) {
+    thoughts(username: $username) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+        createdAt
+        username
+        reactionBody
+      }
+    }
+  }
+`
