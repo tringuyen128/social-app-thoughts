059734495a631dc8e9c914a0189ff7d417c63926
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-06-07 07:04:06 -0700

    mutation completed

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,4 @@
+const { AuthenticationError } = require('apollo-server-express')
 const { User, Thought } = require('../models')
 
 const resolvers = {
@@ -22,6 +23,27 @@ const resolvers = {
         .select('-__v -password')
         .populate('friends')
         .populate('thoughts')
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args)
+      return user
+    },
+
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email })
+
+      if (!User) {
+        throw new AuthenticationError('Incorrect credentials')
+      }
+      const correctPw = await user.isCorrectPassword(password)
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials')
+      }
+      return user
     },
   },
 }
